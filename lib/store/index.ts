52a419df080fe90e50bbb6b275f75514ce2d44f2
Michael Zhang <mail@mzhang.io>
2022-11-07 01:32:41 -0600

    redux wip

---
+++ lib/store/index.ts
@@ -0,0 +1,22 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+export function makeStore() {
+  return configureStore({
+    reducer: {},
+  });
+}
+
+const store = makeStore();
+
+export type AppState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action<string>
+>;
+
+export default store;
