fa704809ec35b6297c30aa0c87510c1ffca65040
Michael Zhang <mail@mzhang.io>
2022-11-07 00:20:22 -0600

    socket implementation wip

---
+++ pages/api/socket.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Server } from "socket.io";
+
+const io = new Server({});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+    res.end();
+    return;
+  }
+
+  console.log("Socket is initializing");
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+
+  io.on("connection", (socket) => {
+    console.log("Received new connection");
+
+    socket.on("input-change", (msg) => {
+      socket.broadcast.emit("update-input", msg);
+    });
+  });
+  res.end();
+}
