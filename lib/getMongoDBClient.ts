f5fd220249cb96615fc73af909d4781bc2e898be
Devin Deng <devindenggithub@gmail.com>
2022-11-06 17:14:23 -0800

    use port env variable for mongodb client

--- lib/getMongoDBClient.ts
+++ lib/getMongoDBClient.ts
@@ -3,10 +3,11 @@ import { Db, MongoClient } from 'mongodb';
 const USERNAME = process.env.MONGO_USERNAME
 const PASSWORD = process.env.MONGO_PASSWORD
 const HOSTNAME = process.env.MONGO_HOSTNAME
-const DATABASENAME = process.env.MONGO_DATABASE_NAME
+const DATABASE_NAME = process.env.MONGO_DATABASE_NAME
+const DATABASE_PORT = process.env.MONGO_DATABASE_PORT
 
 const URI =
-  `mongodb://${USERNAME}:${PASSWORD}@${HOSTNAME ?? 'localhost'}:3001`;
+  `mongodb://${USERNAME}:${PASSWORD}@${HOSTNAME ?? 'localhost'}:${DATABASE_PORT}`;
 
 
 let db: Db | null = null;
@@ -18,7 +19,7 @@ export const getMongoDBClient = async () => {
   const client = new MongoClient(URI);
 
   await client.connect();
-  db = client.db(DATABASENAME);
+  db = client.db(DATABASE_NAME);
   
   return db;
 }
