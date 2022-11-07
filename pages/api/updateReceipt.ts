4a957bce10ccff5ba35e728a5b257c3cd9c8c5c4
Devin Deng <devindenggithub@gmail.com>
2022-11-06 16:53:32 -0800

    remove console.logs, POST only for updateReceipt

--- pages/api/updateReceipt.ts
+++ pages/api/updateReceipt.ts
@@ -6,10 +6,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Record<string, string>[] | Record<string, string>>
 ) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({message: 'Only POST method allowed'});
+  }
   const receipt = req.body;
   const client = await getMongoDBClient();
-
-  console.log('called', receipt);
 
   try {
     // TODO implement this
