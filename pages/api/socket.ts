eedf03896300da8ece96058136df0eaeb33d95d9
Devin Deng <devindenggithub@gmail.com>
2022-11-08 21:20:15 -0800

    psuhign what I have

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -7,22 +7,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (res.socket.server.io) {
-    console.log("Socket is already running");
-    res.end();
-    return;
-  }
 
-  console.log("Socket is initializing");
-  const io = new Server(res.socket.server);
-  res.socket.server.io = io;
+  const receiptId = req.body;
 
-  io.on("connection", (socket) => {
-    console.log("Received new connection");
+  console.log(receiptId);
 
-    socket.on("input-change", (msg) => {
-      socket.broadcast.emit("update-input", msg);
-    });
-  });
+  // if (res.socket?.server.io) {
+  //   console.log("Socket is already running");
+  //   res.end();
+  //   return;
+  // }
+
+  // console.log("Socket is initializing");
+  // const io = new Server(res.socket.server);
+  // res.socket.server.io = io;
+
+  // io.on("connection", (socket) => {
+  //   console.log("Received new connection");
+
+  //   socket.on("input-change", (msg) => {
+  //     socket.broadcast.emit("update-input", msg);
+  //   });
+  // });
+
+  res.status(200);
   res.end();
 }
